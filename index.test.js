a4fa0342a92aecec9ebad78ac6e17a21ada341cf
Sloane Perrault <sloane.perrault@gmail.com>
2017-10-15 12:56:37 -0400

    feat(env): Use minified polyfill in production
    Uses NODE_ENV to detect production build and automatically selects the minified polyfill

--- index.test.js
+++ index.test.js
@@ -6,27 +6,54 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackPolyfillIOPlugin = require('./index')
 
-const OUTPUT_DIR = path.join(__dirname, 'dist')
-
 describe('HtmlWebpackPolyfillIOPlugin', () => {
+  const OUTPUT_DIR = path.join(__dirname, 'dist')
+
   beforeEach(done => {
     rimraf(OUTPUT_DIR, done)
   })
-  it('adds the polyfill.io script to the html page', done => {
-    const compiler = webpack(
-      {
-        entry: path.join(__dirname, 'fixtures', 'index.js'),
-        output: { path: OUTPUT_DIR },
-        plugins: [new HtmlWebpackPlugin(), new HtmlWebpackPolyfillIOPlugin()],
-      },
-      err => {
-        expect(err).toBeFalsy()
-        const html = fs
-          .readFileSync(path.join(OUTPUT_DIR, 'index.html'))
-          .toString()
-        expect(html).toMatchSnapshot()
-        done()
-      }
-    )
+  describe('process.env.NODE_ENV === "production"', () => {
+    beforeAll(() => {
+      process.env.NODE_ENV = 'production'
+    })
+    it('adds the minified polyfill.io script to the html page', done => {
+      const compiler = webpack(
+        {
+          entry: path.join(__dirname, 'fixtures', 'index.js'),
+          output: { path: OUTPUT_DIR },
+          plugins: [new HtmlWebpackPlugin(), new HtmlWebpackPolyfillIOPlugin()],
+        },
+        err => {
+          expect(err).toBeFalsy()
+          const html = fs
+            .readFileSync(path.join(OUTPUT_DIR, 'index.html'))
+            .toString()
+          expect(html).toMatchSnapshot()
+          done()
+        }
+      )
+    })
+  })
+  describe('process.env.NODE_ENV !== "production"', () => {
+    beforeAll(() => {
+      process.env.NODE_ENV = 'development'
+    })
+    it('adds the non-minified polyfill.io script to the html page', done => {
+      const compiler = webpack(
+        {
+          entry: path.join(__dirname, 'fixtures', 'index.js'),
+          output: { path: OUTPUT_DIR },
+          plugins: [new HtmlWebpackPlugin(), new HtmlWebpackPolyfillIOPlugin()],
+        },
+        err => {
+          expect(err).toBeFalsy()
+          const html = fs
+            .readFileSync(path.join(OUTPUT_DIR, 'index.html'))
+            .toString()
+          expect(html).toMatchSnapshot()
+          done()
+        }
+      )
+    })
   })
 })
