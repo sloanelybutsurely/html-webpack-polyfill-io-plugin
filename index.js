e54c0a26a29ce3f1d17a32dfccc427ae7c80b785
Sloane Perrault <sloane.perrault@gmail.com>
2017-10-15 16:19:06 -0400

    test(config): Configure and test config validations

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
 'use strict'
 
 class HtmlWebpackPolyfillIOPlugin {
-  constructor() {}
+  constructor(options = {}) {
+    this.options = {}
+
+    this.options.minify =
+      options.minify !== undefined
+        ? options.minify
+        : process.env.NODE_ENV === 'production' ? true : false
+
+    if (options.features)
+      this.options.features = Array.isArray(options.features)
+        ? options.features.join(',')
+        : options.features
+
+    if (options.excludes)
+      this.options.excludes = Array.isArray(options.excludes)
+        ? options.excludes.join(',')
+        : options.excludes
+
+    if (options.flags) {
+      if (!['always', 'gated'].includes(options.flags))
+        throw new TypeError(
+          `option 'flags' must be one of ['always', 'gated']. Received: '${options.flags}'`
+        )
+
+      this.options.flags = options.flags
+    }
+
+    if (options.callback) {
+      if (!/^[\w\.]+$/.test(options.callback))
+        throw new TypeError(
+          `option 'callback' must match regular expression "^[\w\.]+$". Received: '${options.callback}'`
+        )
+
+      this.options.callback = options.callback
+    }
+
+    if (options.unknown) {
+      if (!['ignore', 'polyfill'].includes(options.unknown))
+        throw new TypeError(
+          `option 'unknown' must be one of ['ignore', 'polyfill']. Received '${options.unknown}'`
+        )
+
+      this.options.unknown = options.unknown
+    }
+
+    if (options.rum !== undefined) {
+      this.options.rum = Boolean(options.rum)
+    }
+  }
+
+  buildSrc() {}
+
+  buildAttributes() {}
+
+  buildTag() {
+    return {
+      tagName: 'script',
+      close: true,
+      attributes: this.buildAttributes(),
+    }
+  }
 
   apply(compiler) {
     compiler.plugin('compilation', compilation => {
       compilation.plugin('html-webpack-plugin-alter-asset-tags', (data, cb) => {
-        const src =
-          process.env.NODE_ENV === 'production'
-            ? 'https://cdn.polyfill.io/v2/polyfill.min.js'
-            : 'https://cdn.polyfill.io/v2/polyfill.js'
-        data.head.push({
-          tagName: 'script',
-          closeTag: true,
-          attributes: { src },
-        })
+        const tag = this.buildTag()
+        data.head.push(tag)
         cb(null, data)
       })
     })
